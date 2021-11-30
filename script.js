5a326504305181eadd8bd45d6189eec7add3b442
Felipe Gomes <felipegomesk7m@gmail.com>
2021-11-30 11:43:05 -0300

    feat.: newCard() e HTML: novo botão

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const message = document.getElementById('message-el');
-const sum = document.getElementById('sum-el');
-
-function startGame() {
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
     
+function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Quer puxar mais uma carta?"
+    } else if (sum === 21) {
+        message = "Vinte e um! Venceu o Jogo!"
+        hasBlackJack = true
+    } else {
+        message = "Você está fora do jogo!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = 8;
+    sum += card;
+    startGame()
 }
